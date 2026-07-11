f175e0c028c2803f0d9410599d12ec4590e03c39
Андрі́й Поланскі <54000164+andriypolanski@users.noreply.github.com>
2026-07-11 00:18:43 -0700

    feat(miner-governor): wire rate-limit + jittered backoff into live write enforcement (#2344) (#4984)

--- packages/gittensory-engine/src/index.ts
+++ packages/gittensory-engine/src/index.ts
@@ -147,6 +147,7 @@ export * from "./governor/rate-limit.js";
 export * from "./governor/budget-cap.js";
 export * from "./governor/self-plagiarism.js";
 export * from "./governor/reputation-throttle.js";
+export * from "./governor/write-rate-limit.js";
 export {
   GOVERNOR_LEDGER_EVENT_TYPES,
   normalizeGovernorLedgerEvent,
